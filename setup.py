d0be4917d0b40b0704c9bfef51f59fda627f2b30
senthil100695 <senthil100695@gmail.com>
2023-12-04 13:24:42 +0530

    code completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'llmapp',
+    version= '0.0.0',
+    author= 'senthil100695',
+    author_email= 'senthil100695@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/helper.py
@@ -0,0 +1,38 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
+os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
+
+def get_pdf_to_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()           
+    return text
+
+def get_text_to_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap = 20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(chunks):
+    embeddings = GooglePalmEmbeddings(google_api_key = os.environ['GOOGLE_API_KEY']  )
+    vector_store = FAISS.from_texts(chunks,embedding = embeddings)
+    return vector_store
+
+def get_conversational_chain(vector_store):
+    llm=GooglePalm(google_api_key = os.environ['GOOGLE_API_KEY'],temperature = 0)
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
